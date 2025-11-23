f50f1ecb5ced388f6b95fa315d834ee83d7dc827
Mariam Mohsen <mariem123mohsen@gmail.com>
2025-11-23 22:43:19 +0200

    Modified Arc , Transformations of silver layer is done successfully✅

--- Docs/DWH_Demo.sql
+++ Docs/DWH_Demo.sql
@@ -6,16 +6,16 @@
 
 --2)Creating Schema for each Layer:
 
---GO
---CREATE SCHEMA Silver;
+GO
+CREATE SCHEMA Silver;
 
---GO
---CREATE SCHEMA Bronze;
+GO
+CREATE SCHEMA Bronze;
 
---GO
---CREATE SCHEMA Gold;
+GO
+CREATE SCHEMA Gold;
 
---GO
+GO
 --Creating Stored Procedure for Bronze Layer:
 CREATE OR ALTER PROCEDURE bronze.load_bronze AS
 BEGIN 
@@ -51,8 +51,48 @@ BEGIN
 	SELECT * FROM Bronze.crm_cust_info;
 
 	SELECT COUNT(*) FROM Bronze.crm_cust_info;
+ 
+    --Data Cleaning: "retreiving the latest updated info per csid
+INSERT INTO silver.crm_cust_info (
+    cst_id,
+    cst_key,
+    cst_firstname,
+    cst_lastname,
+    cst_marital_status,
+    cst_gndr,
+    cst_create_date
+)
+SELECT 
+    cst_id,
+    cst_key,
+    TRIM(cst_firstname) AS cst_first_name,
+    TRIM(cst_lastname) AS cst_last_name,
+	CASE
+	WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
+	WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
+	ELSE 'N/A'
+	END AS cst_marital_status,
+	CASE
+	WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+	WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+	ELSE 'N/A'
+	END AS cst_gender,
+	cst_create_date
+FROM (
+    SELECT 
+        *,
+        ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date) AS cst_flag
+    FROM Bronze.crm_cust_info
+    WHERE cst_id IS NOT NULL
+) AS t
+WHERE cst_flag = 1;
 
+SELECT * FROM silver.crm_cust_info;
 
+	--Data Cleaning: Spaces Check & Removal if needed  
+	SELECT cst_firstname 
+	FROM Bronze.crm_cust_info
+	WHERE cst_firstname != TRIM(cst_firstname)
 
 	-- Creating Product Info table:
 
